6529c4f450161b0ba1d71cb785420984173d3977
zlf0308 <zlf0308@126.com>
2023-01-04 11:09:42 +0800

    checkout

--- shopify.js
+++ shopify.js
@@ -1,10 +1,16 @@
-   
+<script>    
+ window.dataLayer = window.dataLayer || [];                                            
+ 
 {% if first_time_accessed %}
-  <script>
-    gtag('event', 'newPurchase', {
-      'value': {{ checkout.subtotal_price | divided_by: 100.0 }},
-      'currency': '{{ currency }}',
-      'transaction_id': '{{ order_number }}',
-    });
-  </script>
+  window.dataLayer.push({
+       'page_type': 'purchase',
+       'event': 'newPurchase',
+       'value': '{{ checkout.subtotal_price | divided_by: 100.0 }}',
+       'currency': '{{ currency }}',
+       'transaction_id': '{{ order_number }}',
+       'totalValue': '{{total_price | money_without_currency }}',
+       'shipping': '{{shipping_price | money_without_currency }}',
+       'tax': '{{tax_price | money_without_currency }}',
+  });
 {% endif %}
+</script>
